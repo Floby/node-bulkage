09605fe14f113bf920fa92f5282016674905190c
Florent Jaby <florent.jaby@gmail.com>
2020-01-23 22:55:12 +0100

    Checks on size of bulked result

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,103 @@
+import deepEqual = require('fast-deep-equal')
 import Deferred from './deferred'
 
-type DebulkedResult<T> = Promise<T>
-type BulkedResult<T> = Promise<T[]>
+function Bulkage<A, R> (resolver: Bulkage.BulkResolver<A, R>): Bulkage.Bulkage<A, R> {
+  return Bulkage.create<A, R>(resolver)
+}
+
+namespace Bulkage {
+  export interface Bulkage<A, R> {
+    (...args: CallArguments<A>): DebulkedResult<R>
+  }
+  export interface BulkResolver<A, R> {
+    (bulk: Bulk<A>): BulkedResult<R>
+  }
+
+  type DebulkedResult<T> = Promise<T>
+  type Bulk<A> = CallArguments<A>[]
+
+  type BulkedResult<T> = Promise<T[]> | T[]
+
   type CallArguments<T> = T[]
-
-interface PendingCall<A, R> {
-  args: CallArguments<A>,
-  deferred: Deferred<R>[]
-}
-
-type Bulk<A> = CallArguments<A>[]
-export interface BulkResolver<A, R> {
-  (bulk: Bulk<A>): BulkedResult<R>
-}
-
-export interface Bulkage<A, R> {
-  (...args: CallArguments<A>): DebulkedResult<R>
-}
-
-
-function MakeBulkage<A, R> (callable: BulkResolver<A, R>): Bulkage<A, R> {
-  if (!callable) {
-    throw Error('Bulkage MUST be constructed with a callbale argument')
+  interface PendingCall<A, R> {
+    args: CallArguments<A>,
+    deferred: Deferred<R>[]
   }
-  let scheduled = false
-  let pendingCalls: PendingCall<A, R>[] = []
-  return (arg) => {
-    const deferred = new Deferred<R>()
-    const isBulkScheduled = pendingCalls.length >= 1
-    addPendingCall([arg], deferred)
-    if (!isBulkScheduled) {
-      scheduleBulk()
+
+  export function create<A, R> (callable: BulkResolver<A, R>): Bulkage<A, R> {
+    if (!callable || typeof callable !== 'function') {
+      throw Error('Bulkage MUST be constructed with a callbale argument')
     }
-    return deferred.promise
-  }
+    let pendingCalls: PendingCall<A, R>[] = []
 
-  function addPendingCall (args, deferred) {
-    const similarCall = pendingCalls.find((call) => {
-      return call.args[0] === args[0]
-    })
-    if (similarCall) {
-      similarCall.deferred.push(deferred)
-    } else {
-      pendingCalls.push({ args, deferred: [deferred] })
+    return function bulkage (...argsToBulk): Promise<R> {
+      const deferred = new Deferred<R>()
+      const isBulkScheduled = pendingCalls.length >= 1
+      addPendingCall(argsToBulk, deferred)
+      if (!isBulkScheduled) {
+        scheduleBulk()
+      }
+      return deferred.promise
     }
-  }
 
-  async function scheduleBulk () {
-    setImmediate(async () => {
-      const bulkedArgs = pendingCalls.map(({ args }) => args)
-      const bulkedDeferred = pendingCalls.map(({ deferred }) => deferred)
-      pendingCalls = []
+    function addPendingCall (newCallArgs, deferred) {
+      const similarCall = pendingCalls.find(({ args }) => isSimilarCall(args, newCallArgs))
+      if (similarCall) {
+        similarCall.deferred.push(deferred)
+      } else {
+        pendingCalls.push({ args: newCallArgs, deferred: [deferred] })
+      }
+    }
+
+    async function runBulk (bulk: PendingCall<A, R>[]) {
+      const bulkedArgs =  bulk.map(({ args }) => args)
+      const bulkedDeferred = bulk.map(({ deferred }) => deferred)
       const results = await callable(bulkedArgs)
-      results.forEach((result, i) => {
-        bulkedDeferred[i].forEach((deferred) => deferred.resolve(result))
+      if (results.length === bulk.length) {
+        onEachDeferred(bulkedDeferred, (d, i) => d.resolve(results[i]))
+      } else {
+        const error = new BulkedResultSizeError(results.length, bulk.length)
+        onEachDeferred(bulkedDeferred, (d, i) => d.reject(error))
+      }
+    }
+
+    function scheduleBulk () {
+      setImmediate(() => {
+        const bulk = flushPendingCalls()
+        runBulk(bulk)
       })
-    })
+    }
+
+    function flushPendingCalls (): PendingCall<A, R>[] {
+      const bulk = [ ...pendingCalls ]
+      pendingCalls = []
+      return bulk
+    }
+
+    function isSimilarCall (argsLeft, argsRight): boolean {
+      return deepEqual(argsLeft, argsRight)
+    }
+
+  }
+
+  export class BulkedResultSizeError extends Error {
+    readonly actual: number
+    readonly expected: number
+    constructor (actualSize: number, expectedSize: number) {
+      const message = `Resolver gave a bulk result of size ${actualSize} but a result of size ${expectedSize} was expected`
+      super(message)
+      this.actual = actualSize
+      this.expected = expectedSize
+    }
   }
 }
+export default Bulkage
 
-export default MakeBulkage
+
+
+function onEachDeferred<R> (bulkedDeferred: Deferred<R>[][], iterate: (deferred: Deferred<R>, indexInBulk: number) => void) {
+  bulkedDeferred.forEach((deferredList, i) => {
+    deferredList.forEach((deferred) => iterate(deferred, i))
+  })
+}
+
--- test/bulkage.test.ts
+++ test/bulkage.test.ts
@@ -1,69 +1,70 @@
 import * as sinon from 'sinon'
 import { expect } from 'chai'
-import Bulkage, { BulkResolver } from '../src'
+import Bulkage from '../src'
 import delay from 'delay'
 require('chai').use(require('sinon-chai'))
+require('chai').use(require('chai-as-promised'))
 
 describe('Bulkage', () => {
   describe('()', () => {
     it('throws', () => {
-      expect(() => Bulkage(undefined as BulkResolver<any, any>)).to.throw(Error)
+      expect(() => Bulkage(undefined as Bulkage.BulkResolver<any, any>)).to.throw(Error)
     })
   })
 
   describe('(() => void)', () => {
-    let callable
+    let resolver
     beforeEach(() => {
-      callable = sinon.spy((list) => list.map(() => undefined))
+      resolver = sinon.spy((list) => list.map(() => undefined))
     })
     it('returns a function', () => {
-      const bulkage = Bulkage(callable)
+      const bulkage = Bulkage(resolver)
       expect(bulkage).to.be.a('function')
     })
 
     describe('()', () => {
       context('once', () => {
-        it('calls the callable', async () => {
+        it('calls the resolver', async () => {
           // Given
-          const bulkage = Bulkage(callable)
+          const bulkage = Bulkage(resolver)
           // When
           bulkage()
           // Then
           await nextTick()
-          expect(callable).to.have.callCount(1)
+          expect(resolver).to.have.callCount(1)
         })
       })
       context('twice on same tick', () => {
-        it('calls the callable once', async () => {
+        it('calls the resolver once', async () => {
           // Given
-          const bulkage = Bulkage(callable)
+          const bulkage = Bulkage(resolver)
           // When
           bulkage()
           bulkage()
           // Then
           await nextTick()
-          expect(callable).to.have.callCount(1)
+          expect(resolver).to.have.callCount(1)
         })
       })
       context('twice on different ticks', () => {
-        it('calls the callable twice', async () => {
+        it('calls the resolver twice', async () => {
           // Given
-          const bulkage = Bulkage(callable)
+          const bulkage = Bulkage(resolver)
           // When
           bulkage()
           await delay(2)
           bulkage()
           // Then
           await nextTick()
-          expect(callable).to.have.callCount(2)
+          expect(resolver).to.have.callCount(2)
         })
       })
     })
   })
   describe('((ns: number[][]) => Promise<n>)', () => {
-    let callable
+    let resolver
     beforeEach(() => {
-      callable = sinon.spy(async (ns: number[][]) => {
+      resolver = sinon.spy(async (ns: number[][]) => {
         return ns.map(([n]) => n)
       })
     })
@@ -72,7 +73,7 @@ describe('Bulkage', () => {
         it('resolves n', async () => {
           // Given
           const n = 8
-          const bulkage = Bulkage(callable)
+          const bulkage = Bulkage(resolver)
           // When
           const actual = await bulkage(n)
           // Then
@@ -84,7 +85,7 @@ describe('Bulkage', () => {
         const n2 = 18
         it('resolves n for each', async () => {
           // Given
-          const bulkage = Bulkage(callable)
+          const bulkage = Bulkage(resolver)
           // When
           const [ a1, a2 ] = await Promise.all([
             bulkage(n1),
@@ -94,26 +95,37 @@ describe('Bulkage', () => {
           expect(a1).to.equal(n1)
           expect(a2).to.equal(n2)
         })
-        it('calls the callable with a bulk of size 1', async () => {
+        it('calls the resolver with a bulk of size 2', async () => {
           // Given
-          const bulkage = Bulkage(callable)
+          const bulkage = Bulkage(resolver)
           // When
           const [ a1, a2 ] = await Promise.all([
             bulkage(n1),
             bulkage(n2),
           ])
           // Then
-          expect(callable).to.have.been.calledWith([
+          expect(resolver).to.have.been.calledWith([
             [n1],
             [n2]
           ])
         })
-
+        context('when resolver returns an array of a different size than the bulk', () => {
+          it('rejects all promises', async () => {
+            // Given
+            const resolver = (bulk: [number, number][]) => ([8])
+            const bulkage = Bulkage<number, number>(resolver)
+            // When
+            const [ a1, a2 ] = [ bulkage(n1), bulkage(n2) ]
+            // Then
+            await expect(a1).to.eventually.be.rejectedWith(/Resolver gave a bulk result of size 1 but a result of size 2 was expected/i)
+            await expect(a2).to.eventually.be.rejectedWith(/Resolver gave a bulk result of size 1 but a result of size 2 was expected/i)
+          })
+        })
         context('with same argument', () => {
           it('resolves n for each', async () => {
             // Given
             const n = 8
-            const bulkage = Bulkage(callable)
+            const bulkage = Bulkage(resolver)
             // When
             const [ a1, a2 ] = await Promise.all([
               bulkage(n),
@@ -123,17 +135,17 @@ describe('Bulkage', () => {
             expect(a1).to.equal(n)
             expect(a2).to.equal(n)
           })
-          it('calls the callable with a bulk of size 1', async () => {
+          it('calls the resolver with a bulk of size 1', async () => {
             // Given
             const n = 8
-            const bulkage = Bulkage(callable)
+            const bulkage = Bulkage(resolver)
             // When
             const [ a1, a2 ] = await Promise.all([
               bulkage(n),
               bulkage(n),
             ])
             // Then
-            expect(callable).to.have.been.calledWith([
+            expect(resolver).to.have.been.calledWith([
               [n]
             ])
           })
@@ -144,7 +156,7 @@ describe('Bulkage', () => {
           // Given
           const n1 = 8
           const n2 = 18
-          const bulkage = Bulkage(callable)
+          const bulkage = Bulkage(resolver)
           // When
           const a1 = await bulkage(n1)
           await delay(2)
@@ -156,8 +168,47 @@ describe('Bulkage', () => {
       })
     })
   })
+  describe('((n, m) => Promise<n+m>)', () => {
+    const resolver = async (bulk: [number, number][]) => bulk.map((args) => sum(...args))
+    let bulkage: Bulkage.Bulkage<number, number>
+    beforeEach(() => {
+      bulkage = Bulkage(resolver)
+    })
+    describe('(n, m)', () => {
+      const n1 = [2, 3]
+      const n2 = [8, 13]
+      const n3 = [8, 10]
+      context('twice on same tick', () => {
+        it('resolves n+m for each', async () => {
+          // When
+          const [ a1, a2 ] = await Promise.all([
+            bulkage(...n1),
+            bulkage(...n2),
+          ])
+          // Then
+          expect(a1).to.equal(sum(...n1))
+          expect(a2).to.equal(sum(...n2))
+        })
+        context('with only first number equal', () => {
+          it('resolves n+m for each', async () => {
+            // When
+            const [ a1, a2 ] = await Promise.all([
+              bulkage(...n2),
+              bulkage(...n3),
+            ])
+            // Then
+            expect(a1).to.equal(sum(...n2))
+            expect(a2).to.equal(sum(...n3))
+          })
+        })
+      })
+    })
+  })
 })
 
+function sum (...args: number[]): number {
+  return args.reduce((sum, n) => sum + n)
+}
 function nextTick(): Promise<void> {
   return new Promise((resolve) => {
     setImmediate(resolve)
